f7a8431c2d9681318c2954377537582509d7e413
k99k5 <k99k5@qq.com>
2025-08-07 10:48:39 +0800

    init

--- src/hyperf/ConfigProvider.php
+++ src/hyperf/ConfigProvider.php
@@ -7,20 +7,20 @@ class ConfigProvider
     /**
      * Register services.
      */
-	public function __invoke(): array
+    public function __invoke(): array
     {
-	    return [
-		    'publish' => [
-			    [
-				    'id' => 'migration',
-				    'description' => 'The config of migration.',
-				    'source' => __DIR__ . '/Migrations/2022_08_03_000000_create_vector_extension.php',
-				    'destination' => BASE_PATH . '/migrations/2022_08_03_000000_create_vector_extension.php',
-			    ],
-		    ],
-		    'listeners' => [
-			    Listener\BootApplicationListener::class,
-		    ],
-	    ];
+        return [
+            'publish' => [
+                [
+                    'id' => 'migration',
+                    'description' => 'The config of migration.',
+                    'source' => __DIR__ . '/Migrations/2022_08_03_000000_create_vector_extension.php',
+                    'destination' => BASE_PATH . '/migrations/2022_08_03_000000_create_vector_extension.php',
+                ],
+            ],
+            'listeners' => [
+                Listener\BootApplicationListener::class,
+            ],
+        ];
     }
 }
