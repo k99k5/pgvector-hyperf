c7bae5f3610012c5eb758933bd91053d77fb843f
k99k5 <k99k5@qq.com>
2025-08-12 10:35:17 +0800

    feat: init

--- src/hyperf/HasNeighbors.php
+++ src/hyperf/HasNeighbors.php
@@ -2,39 +2,32 @@
 
 namespace Pgvector\Hyperf;
 
+use Hyperf\Database\Model\Concerns\HasGlobalScopes;
+use Hyperf\Database\Model\Model;
 use Hyperf\Database\Query\Builder;
 
+/**
+ * @method static nearestNeighbors(string $column, mixed $value, Distance $distance)
+ */
 trait HasNeighbors
 {
-    public function scopeNearestNeighbors(Builder $query, string $column, mixed $value, Distance $distance): void
+    
+    public function scopeNearestNeighbors(\Hyperf\Database\Query\Builder $query, string $column, mixed $value, Distance $distance): void
     {
-        switch ($distance) {
-            case Distance::L2:
-                $op = '<->';
-                break;
-            case Distance::InnerProduct:
-                $op = '<#>';
-                break;
-            case Distance::Cosine:
-                $op = '<=>';
-                break;
-            case Distance::L1:
-                $op = '<+>';
-                break;
-            case Distance::Hamming:
-                $op = '<~>';
-                break;
-            case Distance::Jaccard:
-                $op = '<%>';
-                break;
-            default:
-                throw new \InvalidArgumentException("Invalid distance");
-        }
+        $op = match ($distance) {
+            Distance::L2 => '<->',
+            Distance::InnerProduct => '<#>',
+            Distance::Cosine => '<=>',
+            Distance::L1 => '<+>',
+            Distance::Hamming => '<~>',
+            Distance::Jaccard => '<%>',
+            default => throw new \InvalidArgumentException("Invalid distance"),
+        };
         $wrapped = $query->getGrammar()->wrap($column);
         $order = "$wrapped $op ?";
         $neighborDistance = $distance == Distance::InnerProduct ? "($order) * -1" : $order;
         $vector = is_array($value) ? new Vector($value) : $value;
-
+        
         // ideally preserve existing select, but does not appear to be a way to get columns
         $query->select()
             ->selectRaw("$neighborDistance AS neighbor_distance", [$vector])
@@ -42,14 +35,15 @@ trait HasNeighbors
             ->whereNotNull($column)
             ->orderByRaw($order, [$vector]);
     }
-
-    public function nearestNeighbors(string $column, Distance $distance): Builder
+    
+    public function nearestNeighbors(string $column, Distance $distance): \Hyperf\Database\Model\Builder
     {
         $id = $this->getKey();
         if (!array_key_exists($column, $this->attributes)) {
-	        throw new \Hyperf\Database\Exception\InvalidArgumentException($this, $column);
+            throw new \Hyperf\Database\Exception\InvalidArgumentException($this, $column);
         }
         $value = $this->getAttributeValue($column);
-        return static::whereKeyNot($id)->nearestNeighbors($column, $value, $distance);
+        return static::whereKeyNot($id)
+            ->nearestNeighbors($column, $value, $distance);
     }
 }
