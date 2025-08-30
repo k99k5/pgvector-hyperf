8d1457bbb28c4c99c9ca3cb2294b58e0a84eb021
TigerKK <545551320@qq.com>
2025-08-31 01:06:31 +0800

    Update HasNeighbors.php

--- src/hyperf/HasNeighbors.php
+++ src/hyperf/HasNeighbors.php
@@ -9,11 +9,8 @@ use Hyperf\Database\Query\Builder;
 /**
  * @method static nearestNeighbors(string $column, mixed $value, Distance $distance)
  */
-trait HasNeighbors
-{
-    
-    public function scopeNearestNeighbors(\Hyperf\Database\Query\Builder $query, string $column, mixed $value, Distance $distance): void
-    {
+trait HasNeighbors {
+    public function scopeNearestNeighbors(\Hyperf\Database\Model\Builder $query, string $column, mixed $value, Distance $distance): void {
         $op = match ($distance) {
             Distance::L2 => '<->',
             Distance::InnerProduct => '<#>',
@@ -23,7 +20,7 @@ trait HasNeighbors
             Distance::Jaccard => '<%>',
             default => throw new \InvalidArgumentException("Invalid distance"),
         };
-        $wrapped = $query->getGrammar()->wrap($column);
+        $wrapped = $this->getQuery()->getGrammar()->wrap($column);
         $order = "$wrapped $op ?";
         $neighborDistance = $distance == Distance::InnerProduct ? "($order) * -1" : $order;
         $vector = is_array($value) ? new Vector($value) : $value;
@@ -36,8 +33,7 @@ trait HasNeighbors
             ->orderByRaw($order, [$vector]);
     }
     
-    public function nearestNeighbors(string $column, Distance $distance): \Hyperf\Database\Model\Builder
-    {
+    public function nearestNeighbors(string $column, Distance $distance): \Hyperf\Database\Model\Builder {
         $id = $this->getKey();
         if (!array_key_exists($column, $this->attributes)) {
             throw new \Hyperf\Database\Exception\InvalidArgumentException($this, $column);
